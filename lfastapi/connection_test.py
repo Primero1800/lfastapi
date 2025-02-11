7f88c0cc558d1c5d153e8282effc805a87db19ae
Primero1800 <Primero@inbox.ru>
2025-02-11 18:31:26 +0300

    first

--- lfastapi/connection_test.py
+++ lfastapi/connection_test.py
@@ -1,8 +1,59 @@
+from datetime import datetime
+
 import httpx
 from fastapi import FastAPI, Body, Header, Response
 from httpx import ConnectError
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Tag(BaseModel):
+    tag: str
+    created: datetime
+    secret: str
+
+
+class TagIn(BaseModel):
+    tag: str
+
+
+class TagOut(BaseModel):
+    tag: str
+    created: datetime
+
+
+tags = []
+
+
+@app.post('/', response_model=TagOut, status_code=201)
+def create_tag(tag_in: TagIn) -> TagOut:
+    tag: Tag = Tag(
+        tag=tag_in.tag,
+        created=datetime.now(),
+        secret="shhhh",
+    )
+
+    tags.append(tag)
+
+    return TagOut(
+        tag=tag.tag,
+        created=tag.created,
+    )
+
+
+@app.get('/{number}', response_model=TagOut, status_code=200)
+def get_tag_by_id(number: int) -> TagOut | Response:
+    try:
+        tag = tags[number]
+    except IndexError as error:
+        return Response(status_code=404, content=str(error).encode())
+    return TagOut(
+        tag=tag.tag,
+        created=tag.created
+    )
+
+
 
 
 @app.post('/hi')
@@ -20,7 +71,7 @@ def happy(response: Response, code: str = 'status', status_code: int = 269):
     response.status_code = status_code
     response.headers[code] = str(status_code)
     print(response.headers)
-    return ':)'
+    return datetime.datetime.now().strftime('%Y_%m_%d %H:%m:%s')[:19]
 
 
 def start_server():
